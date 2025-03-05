ef7066952978650941ba25180fb5fad6edc90564
Submarine7274 <acs112111@gm.ntcu.edu.tw>
2025-03-06 02:08:31 +0800

    add commet

--- C++/EX03_09/Account.cpp
+++ C++/EX03_09/Account.cpp
@@ -4,29 +4,29 @@
 #include"Account.h"
 using namespace std;
 
-Account::Account(std::string accountName, int initialBalance)
-    :name{accountName}{
-            if (initialBalance > 0){
+Account::Account(std::string accountName, int initialBalance)   //constructor建構子，傳入兩個參數
+    :name{accountName}{                                         //只有constructor可以這樣寫，叫做member initializer，在函數的body前面放data member跟初始值
+            if (initialBalance > 0){                            //初始存款預設為0，只有傳入參數大於0才會賦值給 balance變數(data member之一)
                 balance = initialBalance;
             }
         }
 
-void Account::setName(std::string accountName){
-        name = accountName;
+void Account::setName(std::string accountName){                 //這函式是設置帳戶名
+        name = accountName;                                     //傳入的參數賦值給name變數(data member之一)
 }
-string Account::getName(){
-        return name;
+string Account::getName(){                                      //這函式是回傳帳戶名
+        return name;                                            //回傳name變數，因為他是private，外部不能直接存取
 }
-void Account::deposit(int depositAmount){
-    if(depositAmount > 0){
-        balance += depositAmount;
+void Account::deposit(int depositAmount){                       //這函式是存款功能 傳入一個整數的參數
+    if(depositAmount > 0){                                      //資料檢查，存款必須大於0
+        balance += depositAmount;                               //傳入的參數加到balance 變數(data member之一)
     }
 }
-void Account::withdraw(int withdrawalAmount){
-    if(withdrawalAmount > 0 && balance >= withdrawalAmount){
-            balance -= withdrawalAmount;
+void Account::withdraw(int withdrawalAmount){                   //這函數是提款功能 一樣會傳入一個整數的參數
+    if(withdrawalAmount > 0 && balance >= withdrawalAmount){    //資料檢查，提款金額大於0，而且帳戶餘額大於等於提款金額
+            balance -= withdrawalAmount;                        //balance變數(data member之一)減掉傳入的參數 
     }
 }
-int Account::getBalance(){
-    return balance;
+int Account::getBalance(){                                      //這函式是回傳餘額
+    return balance;                                             //回傳balance變數，因為他是private，外部不能直接存取
 }
--- C++/EX03_09/AccountTest.cpp
+++ C++/EX03_09/AccountTest.cpp
@@ -5,44 +5,44 @@
 using namespace std;
 
 int main(void){
-    int withdrawlamount, depositamount;
-    Account account1{"Jane Green", 50};
-    Account account2{"John Blue", 0};
+    int withdrawlamount{0}, depositamount{0};                                                   //宣告提款金額與存款金額變數，等等用到
+    Account account1{"Jane Green", 50};                                                         //宣告Account class的account1物件，並初始化他的值
+    Account account2{"John Blue", 0};                                                           //宣告Account class的account2物件，並初始化他的值
 
-    cout<<"account1: "<<account1.getName()<<" balance is $"<<account1.getBalance()<<endl;
-    cout<<"account2: "<<account2.getName()<<" balance is $"<<account2.getBalance()<<"\n"<<endl;
+    cout<<"account1: "<<account1.getName()<<" balance is $"<<account1.getBalance()<<endl;       //輸出訊息與得到accout1的 name變數值、balance變數值
+    cout<<"account2: "<<account2.getName()<<" balance is $"<<account2.getBalance()<<"\n"<<endl; //輸出訊息與得到accout2的 name變數值、balance變數值
 
-    cout<<"Enter deposit amount for account1: ";
-    cin>>depositamount;
-    account1.deposit(depositamount);
-    cout<<"adding "<<depositamount<<" to account1 balance\n"<<endl;
+    cout<<"Enter deposit amount for account1: ";                                                //輸出提示訊息
+    cin>>depositamount;                                                                         //賦值給depositamount變數
+    account1.deposit(depositamount);                                                            //account1呼叫deposit()函式，並傳入depositamount，用意是存款
+    cout<<"adding "<<depositamount<<" to account1 balance\n"<<endl;                             //輸出提示訊息
 
-    cout<<"account1: "<<account1.getName()<<" balance is $"<<account1.getBalance()<<endl;
-    cout<<"account2: "<<account2.getName()<<" balance is $"<<account2.getBalance()<<endl<<"\n";
+    cout<<"account1: "<<account1.getName()<<" balance is $"<<account1.getBalance()<<endl;       //輸出訊息與得到accout1的 name變數值、balance變數值
+    cout<<"account2: "<<account2.getName()<<" balance is $"<<account2.getBalance()<<endl<<"\n"; //輸出訊息與得到accout2的 name變數值、balance變數值
 
-    cout<<"Enter deposit amount for account2: ";
-    cin>>depositamount;
-    account2.deposit(depositamount);
-    cout<<"adding "<<depositamount<<" to account2 balance\n"<<endl;
+    cout<<"Enter deposit amount for account2: ";                                                //輸出提示訊息
+    cin>>depositamount;                                                                         //賦值給depositamount變數
+    account2.deposit(depositamount);                                                            //account2呼叫deposit()函式，並傳入depositamount，用意是存款
+    cout<<"adding "<<depositamount<<" to account2 balance\n"<<endl;                             //輸出提示訊息
 
-    cout<<"account1: "<<account1.getName()<<" balance is $"<<account1.getBalance()<<endl;
-    cout<<"account2: "<<account2.getName()<<" balance is $"<<account2.getBalance()<<"\n"<<endl;
+    cout<<"account1: "<<account1.getName()<<" balance is $"<<account1.getBalance()<<endl;       //輸出訊息與得到accout1的 name變數值、balance變數值
+    cout<<"account2: "<<account2.getName()<<" balance is $"<<account2.getBalance()<<"\n"<<endl; //輸出訊息與得到accout2的 name變數值、balance變數值
 
-    cout<<"Enter withdrawl amount for account1: ";
-    cin>>withdrawlamount;
-    account1.withdraw(withdrawlamount);
-    cout<<"subtracting "<<withdrawlamount<<" from account1 balance\n"<<endl;
+    cout<<"Enter withdrawl amount for account1: ";                                              //輸出提示訊息
+    cin>>withdrawlamount;                                                                       //賦值給depositamount變數
+    account1.withdraw(withdrawlamount);                                                         //account1呼叫withdraw()函式，並傳入withdrawlamount，用意是提款
+    cout<<"subtracting "<<withdrawlamount<<" from account1 balance\n"<<endl;                    //輸出提示訊息
 
-    cout<<"account1 balance: $"<<account1.getBalance()<<endl;
-    cout<<"account2 balance: $"<<account2.getBalance()<<endl<<"\n";
+    cout<<"account1 balance: $"<<account1.getBalance()<<endl;                                   //輸出訊息與得到accout1的balance變數值
+    cout<<"account2 balance: $"<<account2.getBalance()<<endl<<"\n";                             //輸出訊息與得到accout2的balance變數值
 
-    cout<<"Enter withdrawl amount for account2: ";
-    cin>>withdrawlamount;
-    account2.withdraw(withdrawlamount);
-    cout<<"subtracting "<<withdrawlamount<<" from account2 balance\n"<<endl;
+    cout<<"Enter withdrawl amount for account2: ";                                              //輸出提示訊息
+    cin>>withdrawlamount;                                                                       //賦值給depositamount變數
+    account2.withdraw(withdrawlamount);                                                         //account2呼叫withdraw()函式，並傳入withdrawlamount，用意是提款
+    cout<<"subtracting "<<withdrawlamount<<" from account2 balance\n"<<endl;                    //輸出提示訊息
 
-    cout<<"account1 balance: $"<<account1.getBalance()<<endl;
-    cout<<"account2 balance: $"<<account2.getBalance()<<endl;
+    cout<<"account1 balance: $"<<account1.getBalance()<<endl;                                   //輸出訊息與得到accout1的balance變數值
+    cout<<"account2 balance: $"<<account2.getBalance()<<endl;                                   //輸出訊息與得到accout2的balance變數值
 
-    return 0;
+    return 0;                                                                                   //程式正常結束
 }
