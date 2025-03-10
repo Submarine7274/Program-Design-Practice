d6f5b1a451e6bf01f21a4a92048fff264d898cff
acs112111@gm.ntcu.edu.tw <acs112111@gm.ntcu.edu.tw>
2025-03-10 14:26:18 +0800

    new practice

---
+++ JavaScript:HTML:CSS/todolist.js
@@ -0,0 +1,43 @@
+const input =document.querySelector(".textinput");
+const list = document.querySelector(".list");
+const inputbutton = document.querySelector(".input-button")
+
+function changeList(){
+    if(input.value===""){
+        return;
+    }
+    const task = document.createElement("li");
+    //注意等於
+    task.innerHTML=`                                
+        <input type="checkbox" class="checkbox">
+        <label>${input.value}</label>
+        <button class="trashcan">🗑️</button>
+    `;                                              //注意分號;
+    const trash = task.querySelector(".trashcan");
+    trash.addEventListener("click",function(){
+        task.remove();
+    });
+
+    const check = task.querySelector(".checkbox")
+    check.addEventListener("change",function(){
+        if(check.checked){
+            task.style.textDecoration="line-through";
+            task.style.color="#999";
+            list.appendChild(task);
+        }
+        else{
+            task.style.textDecoration="none";
+            task.style.color="";
+            list.prepend(task);
+        }
+    });
+    list.appendChild(task);
+    input.value ="";
+}
+input.addEventListener("keyup",
+    function(e){
+    if((e.key) === "Enter"){
+        changeList();
+    }
+});
+inputbutton.addEventListener("click",changeList);
