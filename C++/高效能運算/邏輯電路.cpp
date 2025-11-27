3e66e2be9d011e3dc73f9ab049563d7d09e89620
acs112111@gm.ntcu.edu.tw <acs112111@gm.ntcu.edu.tw>
2025-11-27 22:52:35 +0800

    邏輯電路程式更新

--- C++/高效能運算/邏輯電路.cpp
+++ C++/高效能運算/邏輯電路.cpp
@@ -1,14 +1,14 @@
 #include<iostream>
-#ifndef BIT_VALUE
-#define BIT_VALUE(n, i) ( ( n & ( 1 << i ) ) ? 1 : 0 )
+#ifndef BIT_signalValue
+#define BIT_signalValue(n, i) ( ( n & ( 1 << i ) ) ? 1 : 0 )
 #endif
 using namespace std;
 bool check_circuit(int id, int input_digits){
     bool signal[input_digits];
     for (int i = 0; i < input_digits; i++){
-        signal[i] = BIT_VALUE(id, i);
+        signal[i] = BIT_signalValue(id, i);
     }    
-    if ((!signal[0] && !signal[1])||(signal[1] && signal[3])
+    /* if ((!signal[0] && !signal[1])||(signal[1] && signal[3])
     ||(!signal[2] && !signal[3])||(signal[3] && signal[4])
     ||(!signal[4] && signal[5])||(!signal[5] && signal[6])
     ||(!signal[5] && !signal[6])||(!signal[6] && signal[15])
@@ -19,6 +19,21 @@ bool check_circuit(int id, int input_digits){
     ||(signal[13] && signal[14])||(!signal[14] && !signal[15])
     ){
         return 0;// 有一個條件不通過 → return 0
+    } */
+    if (
+        !(
+	(signal[0] || signal[1]) && ( !signal[1] || !signal[3] ) && ( signal[2] || signal[3] ) &&
+	(!signal[3] || !signal[4]) && (signal[4] || !signal[5]) &&
+	(signal[5] || !signal[6] ) && (signal[5] || signal[6]) &&
+	(signal[6] || !signal[15]) && (signal[7] || !signal[8]) &&
+	(!signal[7] || !signal[13]) && (signal[8] || signal[9]) &&
+	(signal[8] || !signal[9]) && (!signal[9] || !signal[10]) &&
+	(signal[9] || signal[11]) && (signal[10] || signal[11]) &&
+	(signal[12] || signal[13]) && (signal[13] || !signal[14]) &&
+	(signal[14] || signal[15])
+        )//early return
+	){
+        return 0;
     }
     cout << id << ") ";
     for (int i = 0; i < input_digits; i++){
