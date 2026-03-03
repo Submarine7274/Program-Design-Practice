c9877b6c040c1fcb690b3ad3489981f34f62a235
acs112111@gm.ntcu.edu.tw <acs112111@gm.ntcu.edu.tw>
2026-03-03 17:27:38 +0800

    JavaScript練習

---
+++ web_front/output練習/outputPractice.js
@@ -0,0 +1,12 @@
+function testInnerHTML(){
+    document.getElementById("innerhtml").innerHTML = "<h2>This is the example of innerHTML method.</h2>";
+}
+function testInnerText(){
+    document.getElementById("innerText").innerText ="This is the example of innerText method.";
+}
+function testDocumentWrite(){
+    document.write("This will oerwrite all of html.");
+}
+function testWindowAlert(){
+    window.alert("Suprise!")
+}
